9d0a788e5406643f9190c44dd11c5cbcd6a57091
alainguillaume <manishalain@hotmail.com>
2024-11-15 01:33:53 +0200

    ft adding views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Book, Library
 # Create your views here.
+
+# Implement Function-based View:
+
+# Create a function-based view in relationship_app/views.py that lists all books stored in the database.
+# This view should render a simple text list of book titles and their authors.
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {'book_list': book_list}
+    return render(request, 'books/book_list.html', context)
