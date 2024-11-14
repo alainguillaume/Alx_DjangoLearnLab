09b252f153309ce09aa6beffc669b9bb8a982b7f
alainguillaume <manishalain@hotmail.com>
2024-11-14 04:47:26 +0200

    fetching books by libraly

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
-from .models import Book, Author
+from .models import Book, Author, Librarian, Library
 # Query all books by a specific author.
 # List all books in a library.
 # Retrieve the librarian for a library.
 books = Book.objects.filter(Author.name)
-books = Book.objects.all()
+books = Book.objects.all(Library.objects.get(name="library_name"),books.all())
 
