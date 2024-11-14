99ceeb803039dfc12b1c9b13d4cfe1422f86a160
alainguillaume <manishalain@hotmail.com>
2024-11-14 04:38:13 +0200

    adding a return statement

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,6 +9,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=300)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='authors')
