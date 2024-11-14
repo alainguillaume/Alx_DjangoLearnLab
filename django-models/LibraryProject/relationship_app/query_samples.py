523507ea0dd0748e12a09745b9dce8d6a50dbbf9
alainguillaume <manishalain@hotmail.com>
2024-11-14 05:23:12 +0200

    fx query #13

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -6,7 +6,7 @@ author = ["Author.objects.get(name=author_name)"]
 books_by_author= ["objects.filter(author=author)"]
 library = ["Library.objects.get(name="library_name")"]
 books = ["Library.objects.get(name=library_name)", "books.all()"]
-librarian = ["Librarian.objects.filter(library=library)"]
+librarian = ["Librarian.objects.get(library=library)"]
 
 
 
