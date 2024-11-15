52921a553ff66e7dfcd09309a0cc767ad4dcd4dc
alainguillaume <manishalain@hotmail.com>
2024-11-15 06:22:47 +0200

    branch merging

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,6 +6,5 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('books/', list_books, name='list_books'), 
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), 
-    path('login/',LoginView.as_view(template_name='relationship_app/login.html', name='login')),
-    path('logut/', LoginView.as_view(template_name='relationship_app/logout.html', name='logout'))
+   
 ]
