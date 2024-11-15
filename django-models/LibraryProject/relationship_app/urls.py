ce9d141ddb941dd34a1f1789eed5f3ad644cee50
alainguillaume <manishalain@hotmail.com>
2024-11-15 07:26:15 +0200

    auth implimentation#10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('books/', list_books, name='list_books'), 
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), 
     path('views.register/', SignUpView.as_view(), name='register'),
-    path('login/', LogInView.as_view(), name='login'),
-    path('logut/', LogOutView.as_view(), name='logout'),
+    path('login/', LogInView.as_view(template_name = 'relationship_app/login.html')),
+    path('logut/', LogOutView.as_view( template_name = 'relationship_app/logout.html')),
 ]
