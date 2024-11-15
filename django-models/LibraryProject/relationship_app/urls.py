1c2ff951a36a348e95c400dd46c0c29895422edc
alainguillaume <manishalain@hotmail.com>
2024-11-15 03:41:56 +0200

    adding templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 
 urlpartens = [
+    path('book/', list_books, name='book'),
+    path('libary/', LibraryDetailView.as_view(), name='libraly'),
 
 ]
