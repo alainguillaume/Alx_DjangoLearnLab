b468c385c8a734e49a6f5f662b28e2ad6652ed20
alainguillaume <manishalain@hotmail.com>
2024-11-17 17:52:29 +0200

    adding user profile view#9

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,9 @@ from .views import list_books, LibraryDetailView, admin_view
 from .views import SignUpView
 from .views import LoginView
 from .views import LogoutView
+from .views import admin_view
+from .views import librarian_view
+from .views import member_view
 
 
 urlpatterns = [
@@ -11,6 +14,9 @@ urlpatterns = [
     path('views.register/', SignUpView.as_view(), name='register'),
     path('login/',  LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('profile/', admin_view, name='profile'),
+    # path('profile/', admin_view, name='profile'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 
 ]
