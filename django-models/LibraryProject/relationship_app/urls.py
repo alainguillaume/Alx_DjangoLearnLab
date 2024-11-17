3d35e79d3ec062e6942d532628b011a43a3d91f0
alainguillaume <manishalain@hotmail.com>
2024-11-17 19:47:02 +0200

    extending book model#2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,10 +6,16 @@ from .views import LogoutView
 from .views import admin_view
 from .views import librarian_view
 from .views import member_view
+from .views import add_book
+from .views import edit_book
+from .views import delete_book
 
 
 urlpatterns = [
     path('books/', list_books, name='list_books'), 
+     path('add_book/', add_book, name='add_book'),  # Add URL for adding a book
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),  # URL for editing a book
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),  # URL for deleting a book
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), 
     path('views.register/', SignUpView.as_view(), name='register'),
     path('login/',  LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
